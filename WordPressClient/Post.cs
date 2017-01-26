03ff72d089836e9fefb1d507104133286a822d62
C. Bess <cbess@quantumquinn.com>
2017-01-25 23:47:33 -0600

    - initial import

---
+++ WordPressClient/Post.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WordPressClient
+{
+	[JsonObject]
+	public class Post
+	{
+		[JsonProperty("id")]
+		public Int64 Id { get; set; }
+
+		[JsonProperty("link")]
+		public string Link { get; set; }
+
+		[JsonIgnore]
+		public string Title
+		{ 
+			get { return GetRenderedString("title"); }
+		}
+
+		[JsonIgnore]
+		public string Html
+		{ 
+			get { return GetRenderedString("content"); }
+		}
+
+		[JsonIgnore]
+		public string Excerpt 
+		{
+			get { return GetRenderedString("excerpt"); }
+		}
+
+		[JsonExtensionData]
+		private IDictionary<string, JToken> AdditionalData { get; set; }
+
+		/// <summary>
+		/// Gets the rendered string from the AdditionalData.
+		/// </summary>
+		/// <returns>The rendered string.</returns>
+		/// <param name="field">Field name.</param>
+		private string GetRenderedString(string field)
+		{
+			var excerptData = AdditionalData[field] as IDictionary<string, JToken>;
+			return (string)excerptData["rendered"];
+		}
+	}
+}
---
+++ WordPressClient/WordPressClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WordPressClient
+{
+	public class WordPressClient
+	{
+		public HttpClient Client { get; private set; }
+		public Uri SiteUri { get; set; }
+
+		public WordPressClient(String siteUrl) : this(new Uri(siteUrl))
+		{
+			// empty
+		}
+
+		public WordPressClient(Uri siteUri)
+		{
+			SiteUri = siteUri;
+			Client = new HttpClient();
+			Client.DefaultRequestHeaders.Accept.Clear();
+			Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+			Client.MaxResponseContentBufferSize = 256000;
+		}
+
+		/// <summary>
+		/// Gets the blog posts.
+		/// </summary>
+		/// <returns>The posts.</returns>
+		public async Task<List<Post>> GetPosts()
+		{
+			var builder = new UriBuilder(SiteUri);
+			builder.Path = "/wp-json/wp/v2/posts";
+
+			var response = await Client.GetAsync(builder.Uri);
+			if (response.IsSuccessStatusCode)
+			{
+				var content = await response.Content.ReadAsStringAsync();
+				return JsonConvert.DeserializeObject<List<Post>>(content);
+			}
+
+			return null;
+		}
+	}
+}
